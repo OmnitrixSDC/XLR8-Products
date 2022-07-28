a479df9eb355980db0b26430166dbfdbb5fc2c63
Bruce <dieselrabago@gmail.com>
2022-07-28 15:55:19 -0700

    Framework and DB
    Implemented Postgres DB schema
    Implemented ETL Code
    Added more file framework for routes and controllers

---
+++ server/db/index.js
@@ -0,0 +1,20 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+const client = new Client({
+  user: process.env.USER,
+  password: '',
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DBNAME
+});
+
+client.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to MySQL!')
+  }
+});
+
+export const client;
